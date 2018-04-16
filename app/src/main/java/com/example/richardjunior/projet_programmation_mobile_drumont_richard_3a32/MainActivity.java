34536e67a810094e2d3f033e9b7d2e02417c6390
Richy <richardjunior.homsikue@esprit.tn>
2018-04-16 11:24:46 +0200

    Mise à jour

--- app/src/main/java/com/example/richardjunior/projet_programmation_mobile_drumont_richard_3a32/MainActivity.java
+++ app/src/main/java/com/example/richardjunior/projet_programmation_mobile_drumont_richard_3a32/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.richardjunior.projet_programmation_mobile_drumont_richard_3a
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
+
 }
